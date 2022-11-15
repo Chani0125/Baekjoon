75211e7a8f8fd5a85d9c8f85bba4a82755bb860e
Mincho_Park <ap25ap25@naver.com>
2022-11-15 22:56:07 +0900

    Solved

--- Solving/9663.py
+++ Solving/9663.py
@@ -1,40 +1,26 @@
-def NQueen(c, b, n, depth, s):
+def NQueen(b, n, depth):
     if depth == n:
         return 1
     cnt = 0
-    d = [(1, 1), (-1, 1), (-1, -1), (1, -1)]
-    for i in range(s, n):
-        if c[0][i]:
-            continue
-        for j in range(n):
-            if c[1][j]:
-                continue
-            tmp = True
-            for x, y in d:
-                if tmp:
-                    k = 1
-                    dx, dy = i + x, j + y
-                    while 0 <= dx < n and 0 <= dy < n:
-                        if b[dx][dy]:
-                            tmp = False
-                            break
-                        k += 1
-                        dx, dy = i + x * k, j + y * k
-                else:
-                    break
-            if tmp:
-                b[i][j] = True
-                c[0][i] = True
-                c[1][j] = True
-                cnt += NQueen(c, b, n, depth+1, i+1)
-                b[i][j] = False
-                c[0][i] = False
-                c[1][j] = False
-
+    for i in range(n):
+        if not b[depth][i]:
+            for j in range(depth+1, n):
+                b[j][i] = True
+                if i+j-depth < n:
+                    b[j][i+j-depth] = True
+                if i-j+depth >= 0:
+                    b[j][i-j+depth] = True
+            cnt += NQueen(b, n, depth+1)
+            for j in range(depth+1, n):
+                b[j][i] = False
+                if i+j-depth < n:
+                    b[j][i+j-depth] = False
+                if i-j+depth >= 0:
+                    b[j][i-j+depth] = False
+    print(cnt)
     return cnt
 
 
 n = int(input())
 b = [[False for _ in range(n)] for _ in range(n)]
-c = [[False for _ in range(n)] for _ in range(2)]
-print(NQueen(c, b, n, 0, 0))
+print(NQueen(b, n, 0))
