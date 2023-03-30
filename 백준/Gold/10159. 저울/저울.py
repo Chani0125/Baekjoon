62ce982b2fb032da6c2557a50b2c8cbf4e3ff550
Mincho_Park <71829572+Chani0125@users.noreply.github.com>
2023-03-30 14:05:33 +0900

    [Gold IV] Title: 저울, Time: 272 ms, Memory: 34276 KB -BaekjoonHub

---
+++ 백준/Gold/10159. 저울/저울.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+
+graph = [[1, ] * n for _ in range(n)]
+
+for _ in range(m):
+    n1, n2 = map(int, sys.stdin.readline().strip().split())
+    graph[n1-1][n2-1] = 0
+
+for i in range(n):
+    graph[i][i] = 0
+
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] > graph[i][k] + graph[k][j]:
+                graph[i][j] = graph[i][k] + graph[k][j]
+
+for i in range(n):
+    ans = 0
+    for j in range(n):
+        ans += graph[i][j] * graph[j][i]
+    print(ans)
