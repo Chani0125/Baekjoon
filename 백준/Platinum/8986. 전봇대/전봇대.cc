583990644e56f8a1d334b25d10a6efbc6873fe7b
Mincho_Park <71829572+Chani0125@users.noreply.github.com>
2024-03-21 16:16:09 +0900

    [Platinum V] Title: 전봇대, Time: 20 ms, Memory: 2412 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/8986. 전봇대/전봇대.cc
+++ 백준/Platinum/8986. 전봇대/전봇대.cc
@@ -2,12 +2,15 @@
 
 using namespace std;
 
-long long cost(vector<int> &x, int p)
+unsigned long long cost(vector<int> &x, unsigned long long p)
 {
-    long long c = 0;
+    unsigned long long c = 0;
     for (int i = 1; i < x.size(); i++)
     {
-        c += abs(x[i] - p*i);
+        if (x[i] >= p*i)
+            c += x[i] - p*i;
+        else
+            c += p*i - x[i];
     }
     return c;
 }
@@ -22,9 +25,15 @@ int main(void)
     for (int i = 0; i < n; i++)
         cin >> x[i];
     
-    int lo = 0, hi = 10000;
-    int p, q;
-    long long cp, cq, c_min, c_now;
+    if (n == 1)
+    {
+        cout << 0 << "\n";
+        return 0;
+    }
+
+    unsigned long long lo = 0, hi = __INT_MAX__;
+    unsigned long long p = lo, q = hi;
+    unsigned long long cp, cq, c_min, c_now;
     while (hi - lo >= 3)
     {
         p = (lo * 2 + hi) / 3;
