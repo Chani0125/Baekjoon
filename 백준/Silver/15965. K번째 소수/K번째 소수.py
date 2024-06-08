34ff1b6b57730ec038835d3a5320f30c55a535c3
Mincho_Park <ap25ap25@naver.com>
2024-06-08 14:52:08 +0900

    [Silver II] Title: K번째 소수, Time: 2980 ms, Memory: 128508 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.py
+++ 백준/Silver/15965. K번째 소수/K번째 소수.py
@@ -6,6 +6,9 @@ while len(primes) < k:
     for i in primes:
         if n % i == 0:
             break
+        if i ** 2 > n:
+            primes.append(n)
+            break
     else:
         primes.append(n)
     n += 1
