88da9c27980d6ca10fba12e19e7c96ff3f087a4b
Mincho_Park <ap25ap25@naver.com>
2024-07-15 23:53:41 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 244 ms, Memory: 183088 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -3,8 +3,16 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 v = sorted(list(map(int, input().split())))
+u = [v[-1-i]-v[i] for i in range((n+1)>>1)]
+
+pow2 = [1]
+for _ in range(300000):
+    pow2.append((pow2[-1] << 1) % 1000000007)
 
 a = 0
-for i in range(n):
-    a += ((1<<i) - (1<<(n-1-i))) * v[i]
-print(a%1000000007)
+for i in range((n+1)>>1):
+    a += (pow2[n-1-i]-pow2[i]) * u[i]
+    a %= 1000000007
+if a < 0:
+    a += 1000000007
+print(a)
