9aa40ddfe4a310ec9231b608dbd5b808b9f7e3a7
Mincho_Park <ap25ap25@naver.com>
2024-07-18 15:21:48 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -0,0 +1,5 @@
+a = [int(input()) for _ in range(3)]
+if sum(a) != 180: print('Error')
+elif a[0] == a[1] == a[2]: print('Equilateral')
+elif a[0] == a[1] or a[1] == a[2] or a[2] == a[0]: print('Isosceles')
+else: print('Scalene')
