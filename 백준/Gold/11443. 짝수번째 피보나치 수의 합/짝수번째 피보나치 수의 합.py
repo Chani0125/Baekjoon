ff154c4c81449c3bc6fb7c9dc42a93b96d6f7b47
Mincho_Park <ap25ap25@naver.com>
2024-08-18 17:50:14 +0900

    [Gold II] Title: 짝수번째 피보나치 수의 합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11443. 짝수번째 피보나치 수의 합/짝수번째 피보나치 수의 합.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+fibo_mat = [[[1, 1], [1, 0]]]
+
+DIVIER = 1000000007
+
+def inner_product(mat1, mat2):
+    a = (mat1[0][0] * mat2[0][0] + mat1[0][1] * mat2[1][0]) % DIVIER
+    b = (mat1[0][0] * mat2[0][1] + mat1[0][1] * mat2[1][1]) % DIVIER
+    c = (mat1[1][0] * mat2[0][0] + mat1[1][1] * mat2[1][0]) % DIVIER
+    d = (mat1[1][0] * mat2[0][1] + mat1[1][1] * mat2[1][1]) % DIVIER
+    return [[a, b], [c, d]]
+
+if n % 2 == 0: n += 1
+
+for i in range(n.bit_length()):
+    fibo_mat.append(inner_product(fibo_mat[-1], fibo_mat[-1]))
+
+a = [[1, 0], [0, 1]]
+
+for i in range(n.bit_length()):
+    if n & (1 << i):
+        a = inner_product(a, fibo_mat[i])
+print(a[0][1] - 1)
